3d1c59e654de4ac5ec6f3fcece61acc32bc76efc
minocys <harich.leung@gmail.com>
2014-12-31 17:52:12 -0500

    finished player view & songqueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,28 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
+  initialize: function(songs){
+    this.on('add', function(){
+      if(this.length === 1){
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function(){
+      this.shift();
+      if(this.length > 0){
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(){
+      this.remove();
+    })
+
+  },
+
+  playFirst: function(){
+    this.at(0).play();
+  },
 
 });
